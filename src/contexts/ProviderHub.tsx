151ead0217d4c4a36781274f6ce1cdb2b95cc62f
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-06-21 16:58:04 +0200

    add VertexIdProvider to ProviderHub

--- src/contexts/ProviderHub.tsx
+++ src/contexts/ProviderHub.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from 'react'
 import { VerticesProvider } from './vertices/VerticesContext'
+import { VertexIdProvider } from './vertexId/VertexIdContext'
 
 interface ProviderHubProps {
   children: ReactNode
 }
 
 const ProviderHub = ({ children }: ProviderHubProps) => {
-  return <VerticesProvider>{children}</VerticesProvider>
+  return (
+    <VerticesProvider>
+      <VertexIdProvider>{children}</VertexIdProvider>
+    </VerticesProvider>
+  )
 }
 
 export default ProviderHub
