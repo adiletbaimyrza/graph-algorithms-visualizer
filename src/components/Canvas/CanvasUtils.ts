2bda8e5839bd2dc11c47fe527838c91c600a4c5a
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-06-21 16:58:13 +0200

    add CanvasUtils

---
+++ src/components/Canvas/CanvasUtils.ts
@@ -0,0 +1,51 @@
+import IVertex from '../../interfaces/IVertex'
+
+const isOutOfBounds = (
+  x: number,
+  y: number,
+  frameWidth: number,
+  frameHeight: number,
+  padding: number = 0
+) => {
+  return (
+    x - padding < 0 ||
+    y - padding < 0 ||
+    x + padding > frameWidth ||
+    y + padding > frameHeight
+  )
+}
+
+const euclideanDistance = (x1: number, y1: number, x2: number, y2: number) => {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+}
+
+const isVertexPositionValid = (
+  vertex: IVertex,
+  vertices: IVertex[],
+  canvasWidth: number,
+  canvasHeight: number,
+  vertexRadius: number
+): boolean => {
+  const isTooCloseToOtherVertices = vertices.some(
+    (existingVertex) =>
+      euclideanDistance(
+        vertex.x,
+        vertex.y,
+        existingVertex.x,
+        existingVertex.y
+      ) <
+      vertexRadius * 2 + vertexRadius / 4
+  )
+
+  const isVertexOutOfBounds = isOutOfBounds(
+    vertex.x,
+    vertex.y,
+    canvasWidth,
+    canvasHeight,
+    vertexRadius
+  )
+
+  return !(isTooCloseToOtherVertices || isVertexOutOfBounds)
+}
+
+export { isVertexPositionValid }
