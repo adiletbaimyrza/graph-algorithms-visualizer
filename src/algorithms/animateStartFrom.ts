8a20adb154d6e103317e0bed2dbf0893abae8d71
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-06-25 12:31:29 +0200

    fix animation bug in startFrom animation

--- src/algorithms/animateStartFrom.ts
+++ src/algorithms/animateStartFrom.ts
@@ -14,28 +14,37 @@ const animateStartFrom = async (
   const newSteps = steps.slice(stepId)
 
   for (const step of newSteps) {
+    // console.log(stepId, step) // for debugging purposes
+
     if (!isAnimatingGet()) {
       break
     }
 
-    $(`#pseudo-${step.codeLineId}`).removeClass('bg-slate-600')
-    $(`#pseudo-${step.codeLineId}`).addClass('bg-yellow-600')
+    $(`#pseudo-${step.codeLineId}`)
+      .removeClass('bg-slate-600')
+      .addClass('bg-yellow-600')
 
-    if (step.vertexId && step.edgeId) {
-      $(`#circle-${step.vertexId}`).removeClass('fill-slate-400')
-      $(`#circle-${step.vertexId}`).addClass('fill-yellow-200')
-      $(`#line-${step.edgeId}`).removeClass('stroke-green-700')
-      $(`#line-${step.edgeId}`).addClass('stroke-yellow-200')
+    if (step.vertexId !== undefined && step.edgeId !== undefined) {
+      $(`#circle-${step.vertexId}`)
+        .removeClass('fill-slate-400')
+        .addClass('fill-yellow-200')
+
+      $(`#line-${step.edgeId}`)
+        .removeClass('stroke-green-700')
+        .addClass('stroke-yellow-200')
     }
-    if (step.vertexId && !step.edgeId) {
-      $(`#circle-${step.vertexId}`).removeClass('fill-yellow-200')
-      $(`#circle-${step.vertexId}`).addClass('fill-red-700')
+    if (step.vertexId !== undefined && step.edgeId === undefined) {
+      $(`#circle-${step.vertexId}`)
+        .removeClass('fill-yellow-200')
+        .removeClass('fill-slate-400')
+        .addClass('fill-red-700')
     }
 
     await sleep(1)
 
-    $(`#pseudo-${step.codeLineId}`).removeClass('bg-yellow-600')
-    $(`#pseudo-${step.codeLineId}`).addClass('bg-slate-600')
+    $(`#pseudo-${step.codeLineId}`)
+      .removeClass('bg-yellow-600')
+      .addClass('bg-slate-600')
 
     stepIdSet(step.id)
   }
