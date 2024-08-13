89d9012787dfa3adc64837e19c963837c745ccf9
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-13 18:15:30 +0200

    add title to pseudocode

--- src/components/Pseudocode/Pseudocode.tsx
+++ src/components/Pseudocode/Pseudocode.tsx
@@ -3,6 +3,22 @@ import Prism from 'prismjs'
 import '../../prism.css'
 import pseudocodes from './pseudocodes'
 import { useCurrentAlgorithmContext } from '../../store/hooks'
+import { TAlgorithm } from '../../types'
+
+const getAlgorithmTitle = (algorithmType: TAlgorithm): string => {
+  switch (algorithmType) {
+    case 'bfs':
+      return 'Breadth-First-Search Algorithm'
+    case 'dfs':
+      return 'Depth-First-Search Algorithm'
+    case 'dijkstra':
+      return "Dijkstra's Algorithm"
+    case 'kruskal':
+      return "Kruskal's MST Algorithm"
+    case 'prim':
+      return "Prim's MST Algorithm"
+  }
+}
 
 const Pseudocode = () => {
   const currentAlgo = useCurrentAlgorithmContext()
@@ -13,6 +29,9 @@ const Pseudocode = () => {
 
   return (
     <div className="bg-slate-500">
+      <h1 className="text-center text-xl p-2 text-stone-50">
+        {getAlgorithmTitle(currentAlgo.get())}
+      </h1>
       {pseudocodes[currentAlgo.get()].map((codeLine, index) => (
         <div key={index} className="relative">
           <span className="absolute left-0 w-8 text-right pr-2 text-gray-400 text-[0.9em]">
