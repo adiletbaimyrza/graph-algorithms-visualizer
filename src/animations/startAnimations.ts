8590a8873331f53009782779572fd61d9ff191da
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-09-09 09:48:44 +0200

    add turbo speed

--- src/animations/startAnimations.ts
+++ src/animations/startAnimations.ts
@@ -14,17 +14,63 @@ const startAnimations = async (
 ) => {
   const newSteps = steps.slice(stepId)
 
-  for (const step of newSteps) {
+  const superSpeed = false
+
+  let i = 0
+
+  while (i + 2 < newSteps.length) {
     if (!isAnimatingGet()) {
       break
     }
 
-    paintPath(step.vxId!, step.dgId, step.anim, step.vxId2)
-    highlightCode(step.cdId)
+    if (superSpeed) {
+      paintPath(
+        newSteps[i].vxId!,
+        newSteps[i].dgId,
+        newSteps[i].anim,
+        newSteps[i].vxId2
+      )
+      paintPath(
+        newSteps[i + 1].vxId!,
+        newSteps[i + 1].dgId,
+        newSteps[i + 1].anim,
+        newSteps[i + 1].vxId2
+      )
+      paintPath(
+        newSteps[i + 2].vxId!,
+        newSteps[i + 2].dgId,
+        newSteps[i + 2].anim,
+        newSteps[i + 2].vxId2
+      )
+      paintPath(
+        newSteps[i + 3].vxId!,
+        newSteps[i + 3].dgId,
+        newSteps[i + 3].anim,
+        newSteps[i + 3].vxId2
+      )
+      paintPath(
+        newSteps[i + 4].vxId!,
+        newSteps[i + 4].dgId,
+        newSteps[i + 4].anim,
+        newSteps[i + 4].vxId2
+      )
+
+      highlightCode(newSteps[i].cdId)
+      i += 5
+    } else {
+      paintPath(
+        newSteps[i].vxId!,
+        newSteps[i].dgId,
+        newSteps[i].anim,
+        newSteps[i].vxId2
+      )
+      highlightCode(newSteps[i].cdId)
+      i++
+    }
 
     await sleep(speedGet())
 
-    stepIdSet(step.id)
+    stepIdSet(newSteps[i].id)
   }
 }
 
